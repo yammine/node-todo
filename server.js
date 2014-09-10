66f99edb1e4b1009655f5cbaf66c0d38cfb0739a
Chris Yammine <chris.yammine@hotmail.com>
2014-09-09 22:30:30 -0700

    Set up the basic configuration for the app

---
+++ server.js
@@ -0,0 +1,28 @@
+// setting up
+var express        = require('express');
+var app            = express();
+var mongoose       = require('mongoose'); // for mongodb
+var morgan         = require('morgan'); // log results to the console
+var bodyParser     = require('body-parser'); // pull information from html POST
+var methodOverride = require('method-override'); // simulate DELETE and PUT
+var port           = process.env.PORT || 8080
+
+// our db path
+var configDB = require('./config/database.js');
+
+// connecting to our database
+mongoose.connect(configDB.url)
+
+// configuration
+
+app.use(express.static(__dirname + '/public'));                 // setting the static files directory
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api as json
+app.use(methodOverride());
+
+
+// listen with node
+app.listen(port);
+console.log("The magic is happening on port " + port);
