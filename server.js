0c052c9ec3e74e608d2234568b4300b198a23673
Chris Yammine <chris.yammine@hotmail.com>
2014-09-11 16:35:51 -0700

    Add bower

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-// setting up
+// setting up ------------------------------------------------------
+
 var express        = require('express');
 var app            = express();
 var mongoose       = require('mongoose'); // for mongodb
@@ -7,13 +8,15 @@ var bodyParser     = require('body-parser'); // pull information from html POST
 var methodOverride = require('method-override'); // simulate DELETE and PUT
 var port           = process.env.PORT || 8080
 
-// our db path
-var configDB = require('./config/database.js');
+// our db path -----------------------------------------------------
 
-// connecting to our database
-mongoose.connect(configDB.url)
+var database = require('./config/database.js');
 
-// configuration
+// connecting to our database --------------------------------------
+
+mongoose.connect(database.url);
+
+// configuration ---------------------------------------------------
 
 app.use(express.static(__dirname + '/public'));                 // setting the static files directory
 app.use(morgan('dev'));                                         // log every request to the console
@@ -22,15 +25,17 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api as json
 app.use(methodOverride());
 
-// Models
+// Models ----------------------------------------------------------
 
 var Todo = mongoose.model('Todo', {
   text:  String
 });
 
-// Routes
+// Routes ----------------------------------------------------------
+
 require('./routes.js')(app, Todo);
 
-// listen with node
+// listen with node ------------------------------------------------
+
 app.listen(port);
 console.log("The magic is happening on port " + port);
